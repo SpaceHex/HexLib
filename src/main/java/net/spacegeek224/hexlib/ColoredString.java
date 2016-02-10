f964687afb18e6b26ad3c9e599278e2d5ea5dec9
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-10 08:49:43 -0500

    Update ColoredString.java

--- src/main/java/net/spacegeek224/hexlib/ColoredString.java
+++ src/main/java/net/spacegeek224/hexlib/ColoredString.java
@@ -3,10 +3,16 @@ package net.spacegeek224.hexlib;
 import org.bukkit.ChatColor;
 
 public class ColoredString extends String {
-  public String original = "";
-  public String res = "";
+  private String original = "";
+  private String res = "";
   public ColoredString(String s) {
     original = s;
     res = ChatColor.translateAlternateColorCodes('&',s);
+  }
+  public String getResult() {
+    return res;
+  }
+  public String getOriginal() {
+    return original;
   }
 }
