5af6a97846f64cd80604723ad588be7527636571
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-11 18:42:46 -0500

    Update ColoredString.java

--- src/main/java/net/spacegeek224/hexlib/ColoredString.java
+++ src/main/java/net/spacegeek224/hexlib/ColoredString.java
@@ -1,6 +1,19 @@
 package net.spacegeek224.hexlib;
 
 import org.bukkit.ChatColor;
+/**
+ * All coordinates which appear as arguments to the methods of this
+ * Graphics object are considered relative to the translation origin
+ * of this Graphics object prior to the invocation of the method.
+ * All rendering operations modify only pixels which lie within the
+ * area bounded by both the current clip of the graphics context
+ * and the extents of the Component used to create the Graphics object.
+ * 
+ * @author      Sami Shaio
+ * @author      Arthur van Hoff
+ * @version     %I%, %G%
+ * @since       1.0
+ */
 
 public class ColoredString {
   
