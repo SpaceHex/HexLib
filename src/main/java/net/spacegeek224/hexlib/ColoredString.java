b90798a95a0a619c2b668d578db20619b856131c
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-11 18:46:09 -0500

    Update ColoredString.java

--- src/main/java/net/spacegeek224/hexlib/ColoredString.java
+++ src/main/java/net/spacegeek224/hexlib/ColoredString.java
@@ -2,45 +2,24 @@ package net.spacegeek224.hexlib;
 
 import org.bukkit.ChatColor;
 /**
- * All coordinates which appear as arguments to the methods of this
- * Graphics object are considered relative to the translation origin
- * of this Graphics object prior to the invocation of the method.
- * All rendering operations modify only pixels which lie within the
- * area bounded by both the current clip of the graphics context
- * and the extents of the Component used to create the Graphics object.
- * 
- * @author      Sami Shaio
- * @author      Arthur van Hoff
- * @version     %I%, %G%
- * @since       1.0
- */
+* A class to help with ChatColors.
+* @author  Maris Usis
+* @version 0.1
+* @since   2016-02-11
+*/
 
 public class ColoredString {
   
 	private String original;
 	private String res;
-	/**
-	 * Creates a new ColoredString
-	 * 
-	 * @param string string to convert
-	 */
 	public ColoredString(String string) {
 		this.original = string;
 		this.res = ChatColor.translateAlternateColorCodes('&',string);
 	}
-	/**
-	* Creates a new ColoredString
-	* 
-	* @return res
-	*/
 	public String getResult() {
 		return this.res;
 	}
-	/**
-	* Creates a new ColoredString
-	* 
-	* @return original
-	*/
+
 	public String getOriginal() {
 		return this.original;
 	}
